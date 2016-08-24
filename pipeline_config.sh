df93ef22e28f48de73b32947716e44f9f72f66c8
wenchichou <wcchou@broadinstitute.org>
2016-08-24 15:53:56 -0400

    upgrade pipe.sh

--- pipeline_config.sh
+++ pipeline_config.sh
@@ -8,28 +8,29 @@
 ########################################
 
 # directory where source scripts are located
-sourcedir="/g/bork/zeller/for_simon/pipeline_test"
+sourcedir=${PWD}
 # directory where CARD reference database is located
 CARD_dir="${sourcedir}/CARD/"
 CARD_fasta="${CARD_dir}/nucleotide_fasta_protein_homolog_model.fasta" 
 
 
 ##Please add hashtag in the front of "WGS_dir" if you don't run WGS pipeline 
-WGS_dir="/g/bork/zeller/for_simon/review/WGS"
-WGS_First="merged.metagenome.CRNd00001-1.1.fastq"
-WGS_Second="merged.metagenome.CRNd00001-1.2.fastq"
+WGS_dir="/home/unix/wcchou/osUTI/data/dataOSUTI.WGS/merged4lanes/"
+WGS_First="merged.metagenome.CRNd00001-1.1.fastq.gz"
+WGS_Second="merged.metagenome.CRNd00001-1.2.fastq.gz"
 ##Please add hashtag in the front of "RNAseq_dir" if you don't run RNAseq pipeline
-RNAseq_dir="/g/bork/zeller/for_simon/review/RNAseq"
-RNAseq_First="CRNr00001-1.bam.end1.fastq"
-RNAseq_Second="CRNr00001-1.bam.end2.fastq"
+RNAseq_dir="/home/unix/wcchou/osUTI/data/dataOSUTI.RNAseq/v2/"
+RNAseq_First="CRNr00001-1.bam.end1.fastq.gz"
+RNAseq_Second="CRNr00001-1.bam.end2.fastq.gz"
 
 # directory where executable tool files are located
-Bowtie2_dir="/g/bork/zeller/for_simon/review/bowtie2-2.2.1"
-Bedtools_dir="/g/bork/zeller/for_simon/review/bedtools/bedtools2/bin"
-Samtools_dir="/g/bork/zeller/for_simon/review/samtools-1.3.1"
+Bowtie2_dir="/broad/software/free/Linux/redhat_6_x86_64/pkgs/bowtie2_2.2.1/"
+Bedtools_dir="/broad/software/free/Linux/redhat_6_x86_64/pkgs/bedtools_2.17.0/bin/"
+Samtools_dir="/broad/software/free/Linux/redhat_6_x86_64/pkgs/samtools/samtools_1.3/bin/"
 
 # output directory where all outputs will be stored
-output_dir="/g/bork/zeller/for_simon/pipeline_test/output/"
+output_dir="./output/"
+mkdir ${output_dir}
 WGS_outputName="test2.drugRes.CRNd00001"
 RNASeq_outputName="test2.drugRes.CRNr00001"
 ###########
