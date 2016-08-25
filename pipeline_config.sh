d417d829f770db519e3a886ffd2421c0733bf7bd
NIU, SHENG-YONG <niu.shengyong@gmail.com>
2016-08-25 13:27:25 +0200

    Testing dataset output file
    Added output file of testing data set

--- pipeline_config.sh
+++ pipeline_config.sh
@@ -8,20 +8,20 @@
 ########################################
 
 # directory where source scripts are located
-sourcedir="/g/bork/zeller/for_simon/pipeline_test"
+sourcedir="/g/bork/zeller/for_simon/review/ARGMap"
 # directory where CARD reference database is located
 CARD_dir="${sourcedir}/CARD/"
 CARD_fasta="${CARD_dir}/nucleotide_fasta_protein_homolog_model.fasta" 
 
 
 ##Please add hashtag in the front of "WGS_dir" if you don't run WGS pipeline 
-WGS_dir="/g/bork/zeller/for_simon/review/WGS"
-WGS_First="merged.metagenome.CRNd00001-1.1.fastq"
-WGS_Second="merged.metagenome.CRNd00001-1.2.fastq"
+WGS_dir="/g/bork/zeller/for_simon/review/ARGMap/example/WGS"
+WGS_First="WGS.end1.fq.gz"
+WGS_Second="WGS.end2.fq.gz"
 ##Please add hashtag in the front of "RNAseq_dir" if you don't run RNAseq pipeline
-RNAseq_dir="/g/bork/zeller/for_simon/review/RNAseq"
-RNAseq_First="CRNr00001-1.bam.end1.fastq"
-RNAseq_Second="CRNr00001-1.bam.end2.fastq"
+RNAseq_dir="/g/bork/zeller/for_simon/review/ARGMap/example/RNAseq"
+RNAseq_First="RNAseq.end1.fq.gz"
+RNAseq_Second="RNAseq.end2.fq.gz"
 
 # directory where executable tool files are located
 Bowtie2_dir="/g/bork/zeller/for_simon/review/bowtie2-2.2.1"
@@ -29,7 +29,7 @@ Bedtools_dir="/g/bork/zeller/for_simon/review/bedtools/bedtools2/bin"
 Samtools_dir="/g/bork/zeller/for_simon/review/samtools-1.3.1"
 
 # output directory where all outputs will be stored
-output_dir="/g/bork/zeller/for_simon/pipeline_test/output/"
+output_dir="${sourcedir}/output/"
 WGS_outputName="test2.drugRes.CRNd00001"
 RNASeq_outputName="test2.drugRes.CRNr00001"
 ###########
